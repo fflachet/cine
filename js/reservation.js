1b569dde5e11aad7316c2deae1d877402f18d6be
François <flachet.f@gmail.com>
2019-11-06 09:32:49 +0100

    js update

--- js/reservation.js
+++ js/reservation.js
@@ -1,7 +1,44 @@
 $( document ).ready(function() {
-    console.log( "ready!" );
+
+    $('.resa input').change(multiPrice());
 });
 
-$(".res").click(function(){
-	$("#modalRes").modal();
-})
+function multiPrice() {
+       var mult = 0;
+       // for each row:
+       $('.resa').each(function () {
+           // get the values from this row:
+           var $val1 = $('.val1', this).val();
+           var $val2 = $('.val2', this).text();
+           console.log('val1 = ' + $val1);
+           console.log('val2 = ' + $val2);
+
+           var $total = ($val1) * ($val2);
+           $('.total',this).text($total);
+           mult += $total;
+       });
+       $('#grandTotal').text(mult);
+}
+
+var tab = {
+	"adulte" : {
+		"semaine" : 10,
+		"soiree" : 12,
+		"week-end": 12.50
+
+	},
+	"enfant" : {
+		"semaine" : 5,
+		"soiree" : 6,
+		"week-end": 6
+	}
+}
+
+    
+
+
+
+
+
+
+
